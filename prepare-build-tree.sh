d9720d90347bb2c2d188db56ac2ec5aacccd6cfc
Jordan McCarthy <mccarthy@opentechinstitute.org>
2014-01-31 14:35:51 -0500

    Added generalized build tree initialization script

---
+++ prepare-build-tree.sh
@@ -0,0 +1,34 @@
+DOWNLOADS_DIR="/tmp/downloads"
+TREE=$1
+MOUNT_INFO=`mount`
+
+if [ -z $TREE ]; then
+	echo "Specified path, $TREE, does not exist, and hence cannot be initialized as a build tree.  Exiting..."
+        exit 1
+fi
+
+if [[ $MOUNT_INFO != *$TREE* ]]; then 
+	mount -t tmpfs -o size=7g tmpfs $TREE
+fi
+
+if [ -n $DOWNLOADS_DIR ]; then
+	rm -rf $DOWNLOADS_DIR/*
+fi
+
+cd $TREE
+if [ -n $TREE/tmp ]; then
+	rm -rf $TREE/tmp
+fi
+if [ -n $TREE/commotion-router ]; then
+	rm -rf $TREE/commotion-router
+fi
+mkdir -p $TREE/tmp/downloads
+git clone https://github.com/opentechinstitute/commotion-router
+cd $TREE/commotion-router
+./setup.sh
+cd $TREE/commotion-router/openwrt
+sed -i .config -e 's,.*CONFIG_CCACHE.*,CONFIG_CCACHE=y,'
+sed -i .config -e "s,CONFIG_DOWNLOAD_FOLDER=\"\",CONFIG_DOWNLOAD_FOLDER=\"$DOWNLOADS_DIR\","
+make
+make clean
+
