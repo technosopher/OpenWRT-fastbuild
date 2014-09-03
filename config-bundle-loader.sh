4b3394c5bb9ea6395b452831858b2c736e9014de
Jordan McCarthy <mccarthy@opentechinstitute.org>
2014-09-02 20:10:17 -0400

    Tweaked bundle loading logic to support differentiation of hardware and network bundles

--- config-bundle-loader.sh
+++ config-bundle-loader.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-BUNDLES_LIST="$WORKSPACE/bundles.conf"
-BUNDLE_NAMES=()
-BUNDLE_LOCATIONS=()
+NET_BUNDLES_LIST="$WORKSPACE/net_bundles.conf"
+HW_BUNDLES_LIST="$WORKSPACE/hw_bundles.conf"
 
 
-# Takes as a parameter the location of an unpacked config bundle.  
-# Runs the bundle's configure.sh, if it exists.  configure.sh is expected
-# to set the $SRC_DIR variable to the location of the source code directory.
-# Finally, copies the contents of the bundle's "file" folder to SRC_DIR.
 
 function cleanTemp {
 	if [ -d "$TEMP_DIR/$1" ]; then rm -rf "$TEMP_DIR/$1"; fi
 }
 
+# Takes as a parameter the location of an unpacked config bundle.  
+# Runs the bundle's configure.sh, if it exists.  configure.sh is expected
+# to set the $SRC_DIR variable to the location of the source code directory.
+# Finally, copies the contents of the bundle's "file" folder to SRC_DIR.
 function ParseBundle {
-	pushd "$TEMP_DIR"
+	cd "$1"
 	if [[ -x "$1/configure.sh" ]]; then 
 		echo "Running $1/configure.sh..."
 		. $1/configure.sh
 	else SRC_DIR=$1
 	fi
-	if [[ -d "$1/files" ]]; then 
-		echo "Copying $1/files/ to $SRC_DIR/files..."
-		cp -af $1/files/* $SRC_DIR/files
-	fi
+	for directory in "$1"; do
+		if [[ -d "$directory" && -d "$SRC_DIR/$directory" ]]; then 
+			echo "Copying $1/$directory/ to $SRC_DIR/$directory"
+			cp -af $1/$directory/* $SRC_DIR/$directory
+		fi
+	done
 	echo 'Nothing else to do.  Exiting...'
-	popd
 	cd "$SRC_DIR"
 }
 
@@ -53,45 +53,53 @@ function FetchGitBundle {
 }
 function FetchHttpBundle {
 	cleanTemp "$2"
-	pushd "$TEMP_DIR"
+	cd "$TEMP_DIR"
 	wget "$1"
 	file=`echo $1 | sed 's,.*/,,g'`
 	echo 'Http Bundle fetched!'
 	#if [[ "$1" =~ '.tar.gz'  ]]; then 
 	#if [[ "$1" =~ '.tar.bz2' ]]
 	#if [[ "$1" =~ '.zip' ]]
-	popd
 	ParseBundle "$TEMP_DIR/$2"
 }
 
+function ParseBundleList {
+	BUNDLE_NAMES=()
+	BUNDLE_LOCATIONS=()
+	while read bundle; do
+		BUNDLE_NAMES+=(`echo $bundle | sed "s,\s.\+,,"`)
+		BUNDLE_LOCATIONS+=(`echo $bundle | sed "s,^.*\s,,"`)
+	done < $1
 
-while read bundle; do
-        BUNDLE_NAMES+=(`echo $bundle | sed "s,\s.\+,,"`)
-        BUNDLE_LOCATIONS+=(`echo $bundle | sed "s,^.*\s,,"`)
-done < $BUNDLES_LIST
+	i=1
+	for config_bundle in "${BUNDLE_NAMES[@]}"; do
+		echo -e "$((i++)). $config_bundle (`echo ${BUNDLE_LOCATIONS[$i-2]} | sed -e 's,.*#,,'`)"
+	done
 
-i=1
-for config_bundle in "${BUNDLE_NAMES[@]}"; do
-	echo -e "$((i++)). $config_bundle (`echo ${BUNDLE_LOCATIONS[$i-2]} | sed -e 's,.*#,,'`)"
-done
+	i=0
+	while [ $i -lt 1 ]; do
+		read -p "Please enter the number or name of the $2 config bundle you wish to use: " choice
+		if [[ "$choice" =~ ^[0-9]*$ ]] && [ "$choice" -gt 0 ] && [ "$choice" -le ${#BUNDLE_NAMES[@]} ]; then
+			i=$choice
+			choice=${BUNDLE_NAMES[$i-1]} 
+			break
+		elif [[ ${BUNDLE_NAMES[@]} =~ "$choice" ]]; then
+			i=1
+			for config_bundle in ${BUNDLE_NAMES[@]}; do
+				if [ $config_bundle = "$choice" ]; then break; else i=$((i+1)); fi
+			done
+			if [ $i -le ${#BUNDLE_NAMES[@]} ]; then break; else i=0; fi
+		fi
+		echo -e "\nError: Invalid input.  Please type the name of the config bundle or a number in the range of 1-${#BUNDLE_NAMES[@]}.\n\n"
+	done
+	IFS='#' read -a location <<< "${BUNDLE_LOCATIONS[$i-1]}"
+	Fetch${location[0]}Bundle ${location[1]} $choice
+}
 
-i=0
-while [ $i -lt 1 ]; do
-	read -p "Please enter the number or name of the config bundle you wish to use: " choice
-        if [[ "$choice" =~ ^[0-9]*$ ]] && [ "$choice" -gt 0 ] && [ "$choice" -le ${#BUNDLE_NAMES[@]} ]; then
-		i=$choice
-                choice=${BUNDLE_NAMES[$i-1]} 
-                break
-        elif [[ ${BUNDLE_NAMES[@]} =~ "$choice" ]]; then
-		i=1
-		for config_bundle in ${BUNDLE_NAMES[@]}; do
-			if [ $config_bundle = "$choice" ]; then break; else i=$((i+1)); fi
-		done
-		if [ $i -le ${#BUNDLE_NAMES[@]} ]; then break; else i=0; fi
-	fi
-	echo -e "\nError: Invalid input.  Please type the name of the config bundle or a number in the range of 1-${#BUNDLE_NAMES[@]}.\n\n"
-done
-IFS='#' read -a location <<< "${BUNDLE_LOCATIONS[$i-1]}"
-Fetch${location[0]}Bundle ${location[1]} $choice
+ParseBundleList $HW_BUNDLES_LIST "router hardware"
+ParseBundleList $NET_BUNDLES_LIST "network configuration"
 
-
+# Problem: both of these calls deliberately put you in a particular directory by the end.  Which probably shouldn't happen anyway.  
+# Alternate design approach: put all bundles in the same file, with designators indicating what type they are
+# Allow for comment lines in bundles.conf files (ignore lines beginning with # - perhaps with that fancy substitution thing (${VAR#\#}?)
+# Problem: what is the spec for determining what goes into each type of bundle?  
